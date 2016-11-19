7d6f1b041af3edbaf7d2f6082106233648b93251
xmdy <xmdy.pro@gmail.com>
2016-11-19 21:33:31 +0300

    update database settings

--- src/application/settings.py
+++ src/application/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stats'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ibstats',
+        'USER': 'ibstats',
+        'PASSWORD': 'ibstats',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
